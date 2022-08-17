b00e0215031dca15e830c3a079cf14caf96d2549
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2022-08-17 22:31:45 +0100

    Timer animation in progress

---
+++ timer.js
@@ -0,0 +1,51 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
+  }
+
+  start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
+    this.tick();
+    this.timerInterval = setInterval(this.tick, 1 * 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.timerInterval);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      //left side: calls the setter every time we want to update the value
+      //right side: calls the getter to retrieve value
+      this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+}
